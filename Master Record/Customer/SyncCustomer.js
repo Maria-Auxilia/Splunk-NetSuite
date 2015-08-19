8acbabdcd4230f31e650951c687e786d357d2ec6
Vaibhav <vaibhav.a.srivastava@accenture.com>
2015-08-19 12:51:33 +0530

    Testing Merge conflict

--- Master Record/Customer/SyncCustomer.js
+++ Master Record/Customer/SyncCustomer.js
@@ -21,7 +21,7 @@ function SyncCustomer(type){
         var pr1 = r.getFieldValue('total');
         //New Change File End git1
         //New Comment
-
+//Changes to Git through Eclipse
         /*------New Change 8/19-------*/
         
        var test = 1234
