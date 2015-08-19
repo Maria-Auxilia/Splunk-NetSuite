8b52e8be4568ab6924375c3fbc461671ac4362d1
Vaibhav <vaibhav.a.srivastava@accenture.com>
2015-08-19 13:01:42 +0530

    Commit Please

--- Master Record/Customer/SyncCustomer.js
+++ Master Record/Customer/SyncCustomer.js
@@ -23,6 +23,7 @@ function SyncCustomer(type){
         //New Comment
 //Changes to Git through Eclipse
 // Changes on Git direct
+        //ANother Change Eclipse
         /*------New Change 8/19-------*/
         
        var test = 1234
